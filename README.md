91c0d95615a8eec4a5ea27f823319186c397266d
Konstantin Ignatiev <kostya@inet-lab.net>
2021-12-08 13:30:18 -0500

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# kign.github.io
+# Personal page
+
+Static content is generated from Jinja2 template and some JSON sources in `src` directory. 
+Use script `etc/make.py` to update.
--- main.js
+++ main.js
@@ -4,6 +4,13 @@ $(function () {
   $('div#selector input[type="checkbox"]').on('click', function() {
     update ();
   });
+  $('p.tags a').on('click', function () {
+    const tag = $(this).attr('href').substr(1);
+    $('div#selector input:checkbox').each(function () {
+      $(this).prop('checked', this.id === tag);
+    });
+    update ();
+  });
 });
 
 function update () {
